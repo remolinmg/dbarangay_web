c4e9a8aa3e85a62ecb1dc2054e625cbd773b49ee
jerichogipoleo <jerichogipoleo@gmail.com>
2023-10-30 22:20:33 +0800

    download

--- client/src/admin-components/blotter-admin.js
+++ client/src/admin-components/blotter-admin.js
@@ -532,12 +532,11 @@ return (
                             <td>{item.defendant}</td>
                             <td>{item.type}</td>
                             <td>{item.address}</td>
-                            <td><img
-                              style={{ width: "100px", height: "100px" }}
-                              src={require(`../../../server/uploads/blotter/${item.filename}`)}
-                              alt=""
-                              className="business-picture"
-                            /></td>
+                            <td>
+                              <a href={require(`../../../server/uploads/blotter/${item.filename}`)} download={item.filename}>
+                                {item.filename}
+                              </a>
+                            </td>
                             <td>{item.kind}</td>
                             <td>{item.status}</td>
                             <td>
