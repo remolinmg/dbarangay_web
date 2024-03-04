e4d2ea0666b8b2581d1d3b8f4dfd139167feb3db
remolinmg <157328021+remolinmg@users.noreply.github.com>
2024-03-04 13:25:42 +0800

    Update emergencyModel.js

--- server/models/emergencyModel.js
+++ server/models/emergencyModel.js
@@ -1,15 +1,15 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const barangayEmergencySchema = new mongoose.Schema({
   userId: { type: String }, //used UserID to show  data request in the flutter app
 
   currentLocation: {
     type: String,
-},
+  },
 
-residentName:{
-  type: String,
-},
+  residentName: {
+    type: String,
+  },
 
   phoneNumber: {
     type: String,
@@ -18,15 +18,18 @@ residentName:{
   emergencyType: {
     type: String,
   },
-  date:{
+  date: {
     type: Date,
-},
+  },
   status: {
     type: String,
   },
+  emergencyProofImage: {
+    public_id: { type: String, required: true },
+    url: { type: String, required: true },
+  },
 });
 
-const userEmergency = mongoose.model('emergency', barangayEmergencySchema);
+const userEmergency = mongoose.model("emergency", barangayEmergencySchema);
 
 module.exports = userEmergency;
-
